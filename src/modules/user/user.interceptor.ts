d366e497802a49bb2714f3a7bad84c5d42f3831b
lazlanrafar <lazlanrafar@gmail.com>
2025-07-03 11:24:58 +0800

    fix(app): fix user reponse

--- src/modules/user/user.interceptor.ts
+++ src/modules/user/user.interceptor.ts
@@ -3,47 +3,45 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
-} from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable()
 export class UserInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      map(data => {
+      map((data) => {
         // Remove sensitive data from user objects
-        if (data && typeof data === 'object') {
+        if (data && typeof data === "object") {
           return this.removeSensitiveData(data);
         }
         return data;
-      }),
+      })
     );
   }
 
   private removeSensitiveData(data: any): any {
     if (Array.isArray(data)) {
-      return data.map(item => this.removeSensitiveData(item));
+      return data.map((item) => this.removeSensitiveData(item));
     }
 
-    if (data && typeof data === 'object') {
-      if (data.data && Array.isArray(data.data)) {
-        // Handle paginated results
-        return {
-          ...data,
-          data: data.data.map(item => this.removeSensitiveData(item)),
-        };
-      }
+    if (data && typeof data === "object" && !(data instanceof Date)) {
+      // Create a shallow copy without stripping prototypes
+      const cleanData = { ...data };
 
-      // Remove password and other sensitive fields
-      const { password, reset_token, reset_token_expires, ...cleanData } = data;
-      
+      // Remove sensitive fields safely
+      delete cleanData.password;
+      delete cleanData.reset_token;
+      delete cleanData.reset_token_expires;
+
       // Recursively clean nested objects
-      Object.keys(cleanData).forEach(key => {
-        if (cleanData[key] && typeof cleanData[key] === 'object') {
-          cleanData[key] = this.removeSensitiveData(cleanData[key]);
+      for (const key of Object.keys(cleanData)) {
+        const value = cleanData[key];
+        if (value && typeof value === "object") {
+          cleanData[key] = this.removeSensitiveData(value);
         }
-      });
+      }
 
       return cleanData;
     }
